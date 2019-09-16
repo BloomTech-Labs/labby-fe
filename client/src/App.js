14aef20540d35d380cd29bd216190ef8dfc7a637
tjforeman <tylerforeman92@gmail.com>
2019-09-16 13:24:00 -0400

    added mixpanel to track analytics

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import mixpanel from "./helpers/mixpanel";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Hello From Labby
-        </p>
+        <p>Hello From Labby</p>
       </header>
     </div>
   );
 }
+
+mixpanel.track("viewed landing");
 
 export default App;
---
+++ client/src/helpers/mixpanel.js
@@ -0,0 +1,6 @@
+import mixpanel from "mixpanel-browser";
+
+mixpanel.init("c2f1e88e71b447bf187bbe55c12e01af");
+
+export default mixpanel;
+

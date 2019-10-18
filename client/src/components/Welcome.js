bcff6730917ef2782c998782e7b3207928927f2e
Laura Daugherty <lauradaugherty6@gmail.com>
2019-10-18 14:31:08 -0700

    welcome styles

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -1,15 +1,36 @@
 import React from "react";
+import { Button } from "@material-ui/core"
+
+import { makeStyles } from '@material-ui/core/styles';
 
 import Logo from "../images/labby.png";
 
+const useStyles = makeStyles({
+  display: {
+    // display: 'flex'
+  },
+  button: {
+    width: '247px',
+    background: '#222E44',
+    color: 'white',
+    margin: '100px'
+  },
+  image: {
+    width: '506px',
+    height: '506px',
+    marginTop: '314px'
+  }
+});
+
 const Welcome = () => {
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.display}>
       <div>
-        <img src={Logo} alt="labby baby" />
+        <img src={Logo} alt="labby logo" className={classes.image}/>
       </div>
       <div>
-        <button>LEARN MORE</button>
+        <Button variant="contained" className={classes.button}>LEARN MORE</Button>
       </div>
     </div>
   );

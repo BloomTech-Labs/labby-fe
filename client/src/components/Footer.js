1cb902776011990705abe0c9914c1464c10848a5
Laura Daugherty <lauradaugherty6@gmail.com>
2019-10-18 14:30:54 -0700

    footer styles

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,8 +1,21 @@
 import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles({
+  
+  footer: {
+    width: '100%',
+    background: '#222E44',
+    color: 'white',
+  },
+});
 
 const Nav = props => {
+  const classes = useStyles();
+
   return (
-    <footer>
+    <footer className={classes.footer}>
       <div>Labby 2019</div>
     </footer>
   );
